882b448e7ecf566ce1e9185b54b04902272a7d24
Joyoounjin <dbswls060@gmail.com>
2024-11-18 21:03:50 +0900

    [Feat] 테스트 UI 기능 구현
    제이슨 데이터 파싱 형태에 맞게 버튼을 누르면 다음 대사가 입력되고, 다음 다이얼로그 인덱스의 이름과 그에 맞는 대사가 출력됩니다.

--- Assets/Scripts/Data/DialogueData.cs
+++ Assets/Scripts/Data/DialogueData.cs
@@ -8,11 +8,6 @@ public class Dialogue
     public string Question;
     public string[] Options;
     public int[] Results;
-    public override string ToString() 
-    {
-        // 예시로 name과 ID만 출력하도록 정의
-        return $"ID: {ID}, Name: {name}, IsOption: {isOption}, Question: {Question}";
-    }
 }
 
 [System.Serializable]
---
+++ Assets/Scripts/Data/TestUI.cs
@@ -0,0 +1,55 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameTxt;
+    [SerializeField] private TextMeshProUGUI lineTxt;
+    [SerializeField] private Button nextButton;
+
+    public Dialogue[] dialogues;
+    private int currentDialogueIndex = 0; 
+    private int currentLineIndex = 0; 
+
+    private void Start()
+    {
+        DialogueData dialogueData = DataManager.Instance.Parse("TestData");
+        if (dialogueData != null)
+        {
+            dialogues = dialogueData.dialogues;
+            UpdateUI();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        nameTxt.text = dialogues[currentDialogueIndex].name;
+        lineTxt.text = dialogues[currentDialogueIndex].lines[currentLineIndex];
+    }
+    public void ShowNextLine()
+    {
+        currentLineIndex++;
+
+        if (currentLineIndex >= dialogues[currentDialogueIndex].lines.Length)
+        {
+            currentDialogueIndex++;
+            currentLineIndex = 0;
+
+            if (currentDialogueIndex >= dialogues.Length)
+            {
+                Debug.Log("모든 대화가 종료되었습니다.");
+                return;
+            }
+        }
+
+        UpdateUI();
+    }
+}
+
+
+
+
+
+
+
